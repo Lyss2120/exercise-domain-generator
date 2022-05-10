5818eae740cbd901cfff9c97027856334c85464a
Lyss2120 <lys.ivonne2020@gmail.com>
2022-05-09 21:27:22 -0400

    2commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let extensiones = [];
+
+for (let index = 0; index < array.length; index++) {
+    const palabra = array[index];
+    
+}
+
+
+
+
+
+
+
+
+
+/*
+Deberás usar los bucles o loops anidados para mezclar los diferentes valores.
+
+Tus herramientas: Para bucles o loops, concatenación de strings.
+Agrega varios tipos de extensiones por ejemplo: .com, .net, .us, .io, etc.
+Agrega "domain hacks" donde la extension forma parte del dominio, por ejemplo:
+ En lugar de puedes.com el "hack" seria pued.es porque sigue diciendo "puedes"
+  pero se aprovecha de la extension .es.
